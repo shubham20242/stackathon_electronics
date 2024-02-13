4d2e6ddd10e9cb8d77bea134c4624fc3a3a600c4
andrew-taggart <andrew.taggart53@gmail.com>
2024-02-13 14:15:43 -0500

    created tv control, model, seed, server

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const db = require('./db')
 const PORT = process.env.PORT || 3001
 const Controller1 = require('./controllers/Controller1')
 const Controller2 = require('./controllers/Controller2')
+const TvController = require('./controllers/tvController')
 
 const app = express()
 
@@ -13,5 +14,14 @@ app.use(bodyParser.json())
 
 app.get('/', (req, res) => res.send('welcome to our landing page!'))
 
+app.get('/tv', TvController.getAllTvs)
+
+app.get('/tv/:id', TvController.getTvById)
+
+app.post('/tv', TvController.createTv)
+
+app.put('/tv/:id', TvController.updateTv)
+
+app.delete('/tv/:id', TvController.deleteTv)
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
